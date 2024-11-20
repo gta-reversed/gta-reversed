9b45b8fd624ad9baf84ed605a45e3de1d9cb4e23
Kamil Marciniak <KamilMarciniak.Kontakt@gmail.com>
2024-11-20 23:04:27 +0100

    Fix formatting

--- source/game_sa/PlantMgr.h
+++ source/game_sa/PlantMgr.h
@@ -6,27 +6,27 @@ class CEntity;
 class CPlantColEntEntry;
 class CPlantLocTri;
 
-constexpr auto MAX_NUM_PLANT_TRIANGLES     = 256;
-constexpr auto MAX_NUM_PROC_OBJECTS        = 40;
-constexpr auto PLANTS_MAX_DISTANCE         = 100.0f;
-constexpr auto PLANTS_MAX_DISTANCE_SQUARED = PLANTS_MAX_DISTANCE * PLANTS_MAX_DISTANCE;
+constexpr auto MAX_NUM_PLANT_TRIANGLES           = 256;
+constexpr auto MAX_NUM_PROC_OBJECTS              = 40;
+constexpr auto PLANTS_MAX_DISTANCE               = 100.0f;
+constexpr auto PLANTS_MAX_DISTANCE_SQUARED       = PLANTS_MAX_DISTANCE * PLANTS_MAX_DISTANCE;
 constexpr auto PROC_OBJECTS_MAX_DISTANCE         = 340.0f;
 constexpr auto PROC_OBJECTS_MAX_DISTANCE_SQUARED = PROC_OBJECTS_MAX_DISTANCE * PROC_OBJECTS_MAX_DISTANCE;
-constexpr auto PLANTS_ALPHA_MIN_DIST       = 3.0f;
-constexpr auto PLANTS_ALPHA_MAX_DIST       = 60.0f;
+constexpr auto PLANTS_ALPHA_MIN_DIST             = 3.0f;
+constexpr auto PLANTS_ALPHA_MAX_DIST             = 60.0f;
 
 class CPlantMgr {
 public:
     static inline RwTexture* (&PC_PlantTextureTab)[4][4] = *(RwTexture * (*)[4][4])0xC039A0;
     static inline RpAtomic*  (&PC_PlantModelsTab)[4][4]  = *(RpAtomic * (*)[4][4])0xC039F0;
 
-    static inline int16&              m_scanCode                  = *(int16*)0xC03980;
-    static inline CPlantLocTri*&      m_UnusedLocTriListHead      = *(CPlantLocTri**)0xC03984;
-    static inline CPlantLocTri*       (&m_CloseLocTriListHead)[4] = *(CPlantLocTri * (*)[4])0xC03988;
-    static inline CPlantColEntEntry*& m_UnusedColEntListHead      = *(CPlantColEntEntry**)0xC03998;
-    static inline CPlantColEntEntry*& m_CloseColEntListHead       = *(CPlantColEntEntry**)0xC0399C;
-    static inline CRGBA&              m_AmbientColor              = *(CRGBA*)0xC03A44;
-    static inline CPlantLocTri        (&m_LocTrisTab)[MAX_NUM_PLANT_TRIANGLES] = *(CPlantLocTri(*)[MAX_NUM_PLANT_TRIANGLES])0xC03A48;
+    static inline int16&              m_scanCode                                = *(int16*)0xC03980;
+    static inline CPlantLocTri*&      m_UnusedLocTriListHead                    = *(CPlantLocTri**)0xC03984;
+    static inline CPlantLocTri*       (&m_CloseLocTriListHead)[4]               = *(CPlantLocTri * (*)[4])0xC03988;
+    static inline CPlantColEntEntry*& m_UnusedColEntListHead                    = *(CPlantColEntEntry**)0xC03998;
+    static inline CPlantColEntEntry*& m_CloseColEntListHead                     = *(CPlantColEntEntry**)0xC0399C;
+    static inline CRGBA&              m_AmbientColor                            = *(CRGBA*)0xC03A44;
+    static inline CPlantLocTri        (&m_LocTrisTab)[MAX_NUM_PLANT_TRIANGLES]  = *(CPlantLocTri(*)[MAX_NUM_PLANT_TRIANGLES])0xC03A48;
     static inline CPlantColEntEntry   (&m_ColEntCacheTab)[MAX_NUM_PROC_OBJECTS] = *(CPlantColEntEntry(*)[MAX_NUM_PROC_OBJECTS])0xC08E48;
 
 public:
