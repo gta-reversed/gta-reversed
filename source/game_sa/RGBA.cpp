e9b95488a871c132afd2393e9a273e5b08597d9a
Pirulax <patrikjankovics7@gmail.com>
2025-01-24 01:11:23 +0100

    Delete `RGBA.cpp`

--- source/game_sa/RGBA.cpp
+++
@@ -1,9 +0,0 @@
-/*
-    Plugin-SDK file
-    Authors: GTA Community. See more here
-    https://github.com/DK22Pac/plugin-sdk
-    Do not delete this comment block. Respect others' work!
-*/
-#include "StdInc.h"
-
-#include "RGBA.h"
