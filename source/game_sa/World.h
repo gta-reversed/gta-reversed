da80a9b1b2ad256c754560af8321cfd0a24f387e
Franco M <francomaro@gmail.com>
2025-01-26 20:59:31 -0300

    Wrong assert (maxX >= minX && maxY >= minY)

--- source/game_sa/World.h
+++ source/game_sa/World.h
@@ -236,7 +236,9 @@ public:
     */
     template<std::predicate<int32, int32> Fn>
     static bool IterateSectors(int32 minX, int32 minY, int32 maxX, int32 maxY, Fn&& fn) {
-        assert(maxX >= minX && maxY >= minY);
+        if (maxX >= minX && maxY >= minY) {
+            return;
+        }
 
         for (auto y = minY; y <= maxY; ++y) {
             for (auto x = minX; x <= maxX; ++x) {
