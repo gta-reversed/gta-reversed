290ff05b474cf14e9c88cd1f795f27d7299dc96f
Pirulax <patrikjankovics7@gmail.com>
2025-01-26 13:16:01 +0100

    `CPedGroupDefaultTaskAllocatorSitInLeaderCar`

--- source/game_sa/Tasks/Allocators/PedGroup/PedGroupDefaultTaskAllocatorSitInLeaderCar.h
+++ source/game_sa/Tasks/Allocators/PedGroup/PedGroupDefaultTaskAllocatorSitInLeaderCar.h
@@ -3,15 +3,53 @@
 #include <Base.h>
 #include <PluginBase.h>
 #include <reversiblehooks/ReversibleHooks.h>
+#include "CarEnterExit.h"
 
 #include "./PedGroupDefaultTaskAllocator.h"
+#include "TaskComplexEnterCarAsDriver.h"
+#include "TaskComplexEnterCarAsPassenger.h"
+#include "TaskSimpleCarDrive.h"
 
 class NOTSA_EXPORT_VTABLE CPedGroupDefaultTaskAllocatorSitInLeaderCar final : public CPedGroupDefaultTaskAllocator {
 public:
     /* no virtual destructor */
 
-    void                              AllocateDefaultTasks(CPedGroup* pedGroup, CPed* ped) const override { plugin::CallMethod<0x5F6FC0>(this, pedGroup, ped); };
-    ePedGroupDefaultTaskAllocatorType GetType() const override { return ePedGroupDefaultTaskAllocatorType::SIT_IN_LEADER_CAR; }; // 0x5F6560
+    // 0x5F6560
+    ePedGroupDefaultTaskAllocatorType GetType() const override { return ePedGroupDefaultTaskAllocatorType::SIT_IN_LEADER_CAR; };
+
+    // 0x5F6FC0
+    void AllocateDefaultTasks(CPedGroup* pedGroup, CPed* ped) const override {
+        const auto leader = pedGroup->GetMembership().GetLeader();
+        if (!leader) {
+            return;
+        }
+        const auto veh = leader->m_pVehicle;
+        if (!veh) {
+            return;
+        }
+        const auto SetPedDefaultTask = [&](CPed* p, int32 i, CTask* task) {
+            auto* const tp = &pedGroup->GetIntelligence().GetDefaultPedTaskPairs()[i];
+            if (tp->m_Ped && (!ped || tp->m_Ped == ped)) {
+                VERIFY(std::exchange(tp->m_Task, task) == nullptr);
+            } else {
+                delete task;
+            }
+        };
+        SetPedDefaultTask(leader, CPedGroupMembership::LEADER_MEM_ID, new CTaskComplexSequence{
+            new CTaskComplexEnterCarAsDriver{veh}, // 0x5F703E
+            new CTaskSimpleCarDrive{veh} // 0x5F7074
+        });
+        size_t seat{};
+        for (auto&& [i, mem] : notsa::enumerate(pedGroup->GetMembership().GetFollowers())) {
+            if (seat >= veh->m_nMaxPassengers) {
+                break;
+            }
+            SetPedDefaultTask(leader, CPedGroupMembership::LEADER_MEM_ID, new CTaskComplexSequence{
+                new CTaskComplexEnterCarAsPassenger{veh, CCarEnterExit::ComputeTargetDoorToEnterAsPassenger(veh, seat++)}, // 0x5F714B
+                new CTaskSimpleCarDrive{veh} // 0x5F7184
+            });
+        }
+    };
 
 public:
     static inline void InjectHooks() {
