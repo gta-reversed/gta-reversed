19ad9ba8c621b8f3c82e9ff978424c747e252e27
Pirulax <patrikjankovics7@gmail.com>
2025-01-29 00:12:29 +0100

    Refactor `CActiveOccluder`

--- source/game_sa/ActiveOccluder.cpp
+++ source/game_sa/ActiveOccluder.cpp
@@ -2,8 +2,7 @@
 
 #include "ActiveOccluder.h"
 
-void CActiveOccluder::InjectHooks()
-{
+void CActiveOccluder::InjectHooks() {
     RH_ScopedClass(CActiveOccluder);
     RH_ScopedCategoryGlobal();
 
@@ -12,35 +11,25 @@ void CActiveOccluder::InjectHooks()
 }
 
 // 0x71E580
-bool CActiveOccluder::IsPointWithinOcclusionArea(float fX, float fY, float fRadius)
-{
-    if (m_LinesUsed <= 0)
-        return true;
-
-    for (auto i = 0; i < m_LinesUsed; ++i) {
-        auto& line = m_Lines[i];
-        if (!IsPointInsideLine(line.Origin.x, line.Origin.y, line.Delta.x, line.Delta.y, fX, fY, fRadius))
-            return false;
-    }
-
-    return true;
+bool CActiveOccluder::IsPointWithinOcclusionArea(float x, float y, float radius) const {
+    return rng::any_of(GetLines(), [=](auto&& l) {
+        return IsPointInsideLine(l.Origin.x, l.Origin.y, l.Delta.x, l.Delta.y, x, y, radius);
+    });
 }
 
 // 0x71FA40
-bool CActiveOccluder::IsPointBehindOccluder(CVector vecPos, float fRadius)
-{
-    if (m_NumFaces <= 0)
-        return true;
-
+bool CActiveOccluder::IsPointBehindOccluder(CVector pt, float radius) const {
+    const auto& cam = TheCamera.GetPosition();
     for (auto i = 0; i < m_NumFaces; ++i) {
-        const auto& vecCamPos = TheCamera.GetPosition();
+        const auto& normal = m_FaceNormals[i];
+        const auto& offset = m_FaceOffsets[i];
 
-        auto fPosDotVec = DotProduct(vecPos, m_FaceNormals[i]) - m_FaceOffsets[i];
-        auto fCamDotVec = DotProduct(vecCamPos, m_FaceNormals[i]) - m_FaceOffsets[i];
+        const auto ptToNormal  = pt.Dot(normal) - offset;
+        const auto camToNormal = cam.Dot(normal) - offset;
 
-        if (fCamDotVec * fPosDotVec >= 0.0F || fabs(fPosDotVec) < fRadius)
+        if (camToNormal * ptToNormal >= 0.0F || std::fabsf(ptToNormal) < radius) {
             return false;
+        }
     }
-
     return true;
 }
