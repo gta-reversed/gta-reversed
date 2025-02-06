2e37c620e606c779e9742cc343cf000675c9290a
yukani <yukani@zohomail.eu>
2025-02-07 00:42:13 +0300

    zoort

--- conanfile.py
+++ conanfile.py
@@ -27,7 +27,6 @@ class saRecipe(ConanFile):
         deps = CMakeDeps(self)
         deps.generate()
         tc = CMakeToolchain(self)
-        tc.user_presets_path = 'ConanPresets.json'
         tc.generate()
 
         copy(self, "*win32*", os.path.join(self.dependencies["imgui"].package_folder,
