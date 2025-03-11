3dc1517b21bb10ade84025b599d84bf28758b974
yukani <yukani@zohomail.eu>
2025-03-12 00:48:31 +0300

    update

--- source/game_sa/Tasks/TaskTypes/TaskComplexJump.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexJump.cpp
@@ -132,7 +132,7 @@ CTask* CTaskComplexJump::CreateSubTask(eTaskType taskType, CPed* ped) {
         }
     case TASK_COMPLEX_IN_AIR_AND_LAND: {
         auto t = new CTaskComplexInAirAndLand(true, false);
-        if (const auto tc = notsa::dyn_cast_if_present<CTaskSimpleClimb>(m_pSubTask)) {
+        if (const auto tc = notsa::cast<CTaskSimpleClimb>(m_pSubTask)) {
             t->m_bInvalidClimb = tc->GetIsInvalidClimb();
         }
         return t;
