8a8d984fcbe00ab47c5a6c6885c7751b4c0ef478
Pirulax <patrikjankovics7@gmail.com>
2025-03-13 01:44:36 +0100

    Apply suggestions (separate `not-at-all` reversed category, disclaimer for inacccurate percentages)

--- contrib/hooks2md.py
+++ contrib/hooks2md.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 import csv
 import datetime
 import tkinter.filedialog as tkFileDialog
@@ -34,27 +35,60 @@ def main():
             name: str = r["class"]
             klass_info.setdefault(name, Klass(name, 0, 0)).process_row(r)
 
-    with open(output_md, "w", encoding="utf8", newline='\n') as outf:
-        partially: list[Klass]  = []
-        completely: list[Klass] = []
-        for klass in klass_info.values():
-            (completely if klass.is_completely_reversed else partially).append(klass)
-        num_total_klass = len(partially) + len(completely)
+    not_at_all: list[Klass] = []
+    partially: list[Klass]  = []
+    completely: list[Klass] = []
+    for klass in klass_info.values():
+        if klass.num_not_reversed == 0:
+            completely.append(klass)
+        elif klass.num_reversed == 0:
+            not_at_all.append(klass)
+        else:
+            partially.append(klass)
+    num_total_klass = len(partially) + len(completely) + len(not_at_all)
 
+    with open(output_md, "w", encoding="utf8", newline='\n') as outf:
         outf.write(f"# Reversed Classes [As of {datetime.datetime.now(datetime.timezone.utc).strftime('%b %d, %Y, %H:%M:%S')} UTC]\n")
+
+        outf.write("## Disclaimer\n")
+        outf.write(
+            "The percentages and the number of classes shown here may not be "
+            "completely accurate, because not all classes and functions "
+            "are documented yet.\n"
+        )
+
+        #outf.write("## Brief stats\n")
+        #outf.write(f"- #Functions: {sum(k.num_fn for k in klass_info.values())}<br />\n")
+        #outf.write(f"- #Classes: {len(klass_info)}<br />\n")
+        
+        outf.write(f"## Stats ({sum(k.num_fn for k in klass_info.values())} functions, {len(klass_info)} classes)\n")
 
         def write_header(title: str, klasses: list[Klass]):
             outf.write("\n")
-            outf.write(f"## {title} ({len(klasses)}/{num_total_klass}) [{len(klasses) / num_total_klass:.0%}]\n")
+            outf.write(f"#### {title} ({len(klasses)}/{num_total_klass}) [{len(klasses) / num_total_klass:.0%}]\n")
             outf.write("\n")
 
-        write_header("Completely Reversed classes", completely)
-        for klass in completely:
-            outf.write(f"- {klass.name} ({klass.num_reversed})\n")
+        @contextmanager
+        def class_list_spoiler():
+            outf.write("<details>\n")
+            outf.write("<summary>See list of classes</summary>")
+            yield
+            outf.write("\n</details>\n")
 
-        write_header("Partially Reversed classes", partially)
-        for klass in partially:
-            outf.write(f"- {klass.name} ({klass.num_reversed}/{klass.num_fn}) [{1 - klass.num_not_reversed / klass.num_fn:.0%}]\n")
+        write_header("Completely reversed classes", completely)
+        with class_list_spoiler():
+            for klass in completely:
+                outf.write(f"- {klass.name} ({klass.num_fn})<br />\n")
+
+        write_header("Partially reversed classes", partially)
+        with class_list_spoiler():
+            for klass in partially:
+                outf.write(f"- {klass.name} ({klass.num_reversed}/{klass.num_fn}) [{1 - klass.num_not_reversed / klass.num_fn:.0%}]<br />\n")
+
+        write_header("Not-at-all reversed classes", not_at_all)
+        with class_list_spoiler():
+            for klass in not_at_all:
+                outf.write(f"- {klass.name} ({klass.num_fn})<br />\n")
 
 if __name__ == '__main__':
     main()
