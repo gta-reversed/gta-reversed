fdd6a9795cf34057a40ae35a3559dbeb72352da8
Tornamic <kirilltymoshchenko59@gmail.com>
2025-03-15 14:00:17 +0200

    implement CBmx::ProcessControl

--- source/game_sa/Entity/Vehicle/Bmx.cpp
+++ source/game_sa/Entity/Vehicle/Bmx.cpp
@@ -8,6 +8,7 @@ void CBmx::InjectHooks() {
     RH_ScopedVMTInstall(SetUpWheelColModel, 0x6BF9B0);
     RH_ScopedVMTInstall(BurstTyre, 0x6BF9C0);
     RH_ScopedVMTInstall(FindWheelWidth, 0x6C0550);
+    RH_ScopedVMTInstall(ProcessControl, 0x6BFA30);
 }
 
 // 0x6BF820
@@ -67,7 +68,34 @@ bool CBmx::BurstTyre(uint8 tyreComponentId, bool bPhysicalEffect) {
 
 // 0x6BFA30
 void CBmx::ProcessControl() {
-    plugin::CallMethod<0x6BFA30, CBmx*>(this);
+    const float BMX_SPRINT_LEANSTART = FRAC_PI_2;
+    const float BMX_PEDAL_LEANSTART = 0.0f;
+    const float BMX_SPRINT_LEANMULT = 0.3f;
+    const float MTB_SPRINT_LEANMULT = 0.087f;
+    const float BMX_PEDAL_LEANMULT = 0.07f;
+    const float MTB_PEDAL_LEANMULT = 0.02f;
+
+    CBike::ProcessControl();
+
+    if (!m_bWasPostponed || m_nStatus != eEntityStatus::STATUS_PLAYER || !m_pDriver) {
+        return;
+    }
+
+    auto anim = RpAnimBlendClumpGetAssociation(m_pDriver->m_pRwClump, ANIM_ID_BIKE_SPRINT);
+    bool isMountainBike = GetModelId() == MODEL_MTBIKE;
+
+    if (anim && anim->GetBlendAmount() > 0.01f) {
+        float mult = isMountainBike ? MTB_SPRINT_LEANMULT : BMX_SPRINT_LEANMULT;
+        m_fSprintLeanAngle = std::sin(anim->GetCurrentTime() / anim->GetHier()->GetTotalTime() * TWO_PI + BMX_SPRINT_LEANSTART) * anim->GetBlendAmount() * mult;
+    }
+    else {
+        anim = RpAnimBlendClumpGetAssociation(m_pDriver->m_pRwClump, ANIM_ID_BIKE_PEDAL);
+        if (anim && anim->GetBlendAmount() > 0.01f) {
+            float mult = isMountainBike ? MTB_PEDAL_LEANMULT : BMX_PEDAL_LEANMULT;
+            m_RideAnimData.m_fAnimLean += std::sin(anim->GetCurrentTime() / anim->GetHier()->GetTotalTime() * TWO_PI + BMX_PEDAL_LEANSTART) * anim->GetBlendAmount() * mult;
+        }
+        m_fSprintLeanAngle *= 0.95f;
+    }
 }
 
 // 0x6BFB50
