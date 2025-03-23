bb8231a3f7d357c63b80d38a614f20a985a51704
Pirulax <patrikjankovics7@gmail.com>
2025-03-23 01:40:07 +0100

    `CFormation::FindCoverPointsBehindBo`

--- source/InjectHooksMain.cpp
+++ source/InjectHooksMain.cpp
@@ -484,6 +484,7 @@
 #include "EventLeaderQuitEnteringCarAsDriver.h"
 #include "EventAreaCodes.h"
 #include "EventLeaderEntryExit.h"
+#include "Formation.h"
 
 #include "Plugins/BreakablePlugin/BreakablePlugin.h"
 
@@ -505,6 +506,7 @@ void InjectHooksMain() {
     HookInstall(0x541DD0, CPad::UpdatePads); // [ImGui] Changes logic of the function and shouldn't be toggled on/off
     HookInstall(0x459F70, CVehicleRecording::Render); // [ImGui] Debug stuff rendering
 
+    CFormation::InjectHooks();
     CHandShaker::InjectHooks();
     CCutsceneMgr::InjectHooks();
     CFileMgr::InjectHooks();
