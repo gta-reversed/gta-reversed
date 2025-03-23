b6766201f8ecec816467e375b058084aa8fbd4f2
Pirulax <patrikjankovics7@gmail.com>
2025-03-23 21:54:47 +0100

    fix crash because of `CTaskComplexGangFollower::CalculateOffsetPosition`

--- source/game_sa/Tasks/TaskTypes/TaskComplexGangFollower.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexGangFollower.cpp
@@ -8,7 +8,7 @@ void CTaskComplexGangFollower::InjectHooks() {
 
     RH_ScopedInstall(Constructor, 0x65EAA0, { .reversed = false });
     RH_ScopedInstall(Destructor, 0x65EBB0, { .reversed = false });
-    RH_ScopedInstall(CalculateOffsetPosition, 0x65ED40, { .reversed = false });
+    //RH_ScopedInstall(CalculateOffsetPosition, 0x65ED40, { .reversed = false }); // not hooked because i want to keep CVector return, but original function took a CVector&
     RH_ScopedInstall(Clone, 0x65ECB0, { .reversed = false });
     RH_ScopedInstall(MakeAbortable, 0x65EC30, { .reversed = false });
     RH_ScopedInstall(CreateNextSubTask, 0x665E00, { .reversed = false });
@@ -29,7 +29,9 @@ CTaskComplexGangFollower::~CTaskComplexGangFollower() {
 
 // 0x65ED40
 CVector CTaskComplexGangFollower::CalculateOffsetPosition() {
-    return plugin::CallMethodAndReturn<CVector, 0x65ED40, CTaskComplexGangFollower*>(this);
+    CVector ret;
+    plugin::CallMethod<0x65ED40, CTaskComplexGangFollower*>(this, &ret);
+    return ret;
 }
 
 // 0x65ECB0
