34c7b68e0d800815f61d64e4423ed33190b7514c
Franco M <francomaro@gmail.com>
2025-03-31 13:45:20 +0000

    Fixes

--- source/game_sa/MouseControllerState.cpp
+++ source/game_sa/MouseControllerState.cpp
@@ -43,7 +43,7 @@ bool CMouseControllerState::CheckForInput() const {
         || m_bWheelMovedDown
         || m_bMsFirstXButton
         || m_bMsSecondXButton
-        || (m_AmountMoved.x).IsZero()
-        || (m_AmountMoved.y).IsZero()
+        || m_AmountMoved.x != 0.0f
+        || m_AmountMoved.y != 0.0f
     );
 }
