65b92fc08ccbc41d6dbb8f8f4889e9df890d8a53
Franco M <francomaro@gmail.com>
2025-03-31 13:25:23 +0000

    bool8 to bool

--- source/game_sa/MouseControllerState.h
+++ source/game_sa/MouseControllerState.h
@@ -2,13 +2,13 @@
 
 class CMouseControllerState {
 public:
-    bool8 m_bLeftButton; // LMB
-    bool8 m_bRightButton; // RMB
-    bool8 m_bMiddleButton; // MMB
-    bool8 m_bWheelMovedUp; // Wheel up
-    bool8 m_bWheelMovedDown; // Wheel down
-    bool8 m_bMsFirstXButton; // BMX1
-    bool8 m_bMsSecondXButton; // BMX2
+    bool m_bLeftButton; // LMB
+    bool m_bRightButton; // RMB
+    bool m_bMiddleButton; // MMB
+    bool m_bWheelMovedUp; // Wheel up
+    bool m_bWheelMovedDown; // Wheel down
+    bool m_bMsFirstXButton; // BMX1
+    bool m_bMsSecondXButton; // BMX2
     float m_fWheelMoved; // Wheel movement
     CVector2D m_AmountMoved; // Mouse movement
   
