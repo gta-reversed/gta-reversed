693e02a43645b990f1e03a028957a6850ad48d0b
yukani <yukani@zohomail.eu>
2025-03-31 20:51:12 +0300

    remove unnecessary ./ prefix

--- source/game_sa/Scripts/Commands/CLEO/Extensions/Fs.cpp
+++ source/game_sa/Scripts/Commands/CLEO/Extensions/Fs.cpp
@@ -1,6 +1,6 @@
 #include <StdInc.h>
 
-#include "./Commands.hpp"
+#include "Commands.hpp"
 #include <CommandParser/Parser.hpp>
 
 void notsa::script::commands::cleo::extensions::fs::RegisterHandlers() {
