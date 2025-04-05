08470263ded6bb2079cf3e9a8769ed026493b11b
Vlad <denjidananaa@bk.ru>
2025-04-05 21:26:36 +0300

    Patch `default` use `std::min` and `std::max`

--- source/game_sa/Audio/entities/AEWeaponAudioEntity.cpp
+++ source/game_sa/Audio/entities/AEWeaponAudioEntity.cpp
@@ -988,9 +988,7 @@ void CAEWeaponAudioEntity::UpdateParameters(CAESound* sound, int16 curPlayPos) {
         break;
     }
     default: { // 0x504ECD
-        if (sound->m_fVolume > 0.0f) {
-            sound->m_fVolume = std::max(0.0f, sound->m_fVolume - 2.5f);
-        }
+        sound->m_fVolume = std::max(sound->m_fVolume - 2.5f, std::min(sound->m_fVolume, 0.0f));
         break;
     }
     }
