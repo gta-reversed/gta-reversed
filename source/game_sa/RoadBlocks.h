1c944ca55ad683ea84dd25db4dd127651c934280
yukani <yukani@zohomail.eu>
2025-04-08 16:45:37 +0300

    initial

--- source/game_sa/RoadBlocks.h
+++ source/game_sa/RoadBlocks.h
@@ -6,6 +6,18 @@
 class CVehicle;
 class CPed;
 
+struct CRoadBlock {
+public:
+    CVector CornerA;
+    CVector CornerB;
+    bool    IsActive;
+    bool    IsCreated;
+    uint8   Type; // TODO
+private:
+    uint8   __field1B;
+};
+VALIDATE_SIZE(CRoadBlock, 0x1C);
+
 class CRoadBlocks {
 public:
     static void InjectHooks();
@@ -18,4 +30,11 @@ public:
     static void GenerateRoadBlocks();
     static bool GetRoadBlockNodeInfo(CNodeAddress a1, float& a2, CVector& a3);
     static void RegisterScriptRoadBlock(CVector a1, CVector a2, bool a3);
+
+public:
+    static inline std::array<bool, 325>&         InOrOut                   = StaticRef<std::array<bool, 325>>(0xA43438);
+    static inline std::array<CNodeAddress, 325>& RoadBlockNodes            = StaticRef<std::array<CNodeAddress, 325>>(0xA435A0);
+    static inline std::array<CRoadBlock, 16>&    aScriptRoadBlocks         = StaticRef<std::array<CRoadBlock, 16>>(0xA43AB8);
+    static inline bool&                          GenerateDynamicRoadBlocks = StaticRef<bool>(0xA43584);
+    static inline int32&                         NumRoadBlocks             = StaticRef<int32>(0xA43580);
 };
