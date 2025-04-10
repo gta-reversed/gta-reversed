170dcb58899b8ad873f42d6e4d54fb6e182ef490
yukani <yukani@zohomail.eu>
2025-04-10 19:43:57 +0300

    GenerateRoadBlockCopsForCar done

--- source/game_sa/RoadBlocks.cpp
+++ source/game_sa/RoadBlocks.cpp
@@ -1,6 +1,9 @@
 #include "StdInc.h"
 
 #include "RoadBlocks.h"
+#include "PedPlacement.h"
+#include "TaskComplexWanderCop.h"
+#include "TaskSimpleStandStill.h"
 #include <extensions/File.hpp>
 
 void CRoadBlocks::InjectHooks() {
@@ -11,7 +14,7 @@ void CRoadBlocks::InjectHooks() {
     RH_ScopedInstall(ClearScriptRoadBlocks, 0x460EC0);
     RH_ScopedInstall(ClearSpaceForRoadBlockObject, 0x461020);
     RH_ScopedInstall(CreateRoadBlockBetween2Points, 0x4619C0, { .reversed = false });
-    RH_ScopedInstall(GenerateRoadBlockCopsForCar, 0x461170, { .reversed = false });
+    RH_ScopedInstall(GenerateRoadBlockCopsForCar, 0x461170);
     RH_ScopedInstall(GenerateRoadBlocks, 0x4629E0, { .reversed = false });
     RH_ScopedInstall(GetRoadBlockNodeInfo, 0x460EE0, { .reversed = false });
     RH_ScopedInstall(RegisterScriptRoadBlock, 0x460DF0);
@@ -131,42 +134,93 @@ void CRoadBlocks::GenerateRoadBlockCopsForCar(CVehicle* vehicle, int32 pedsPosit
             copType = COP_TYPE_CITYCOP;
             break;
         }
-    } else {
-        if (IsPedTypeGang(type)) {
-            bool found{};
-            for (auto i = 0; i < TOTAL_GANGS; i++) {
-                if (CPopCycle::m_pCurrZoneInfo->GangStrength[i]) {
-                    pedModel = CGangs::ChooseGangPedModel((eGangID)i);
-                    if (pedModel != MODEL_INVALID) {
-                        found = true;
-                        break;
-                    }
+    } else if (IsPedTypeGang(type)) {
+        bool found{};
+        for (auto i = 0; i < TOTAL_GANGS; i++) {
+            if (CPopCycle::m_pCurrZoneInfo->GangStrength[i]) {
+                pedModel = CGangs::ChooseGangPedModel((eGangID)i);
+                if (pedModel != MODEL_INVALID) {
+                    found = true;
+                    break;
                 }
             }
+        }
 
-            if (!found) {
-                return;
-            }
+        if (!found) {
+            return;
         }
     }
 
-    static constexpr CVector PLACEMENTS[] = {
+    static constexpr auto PLACEMENTS = std::to_array<CVector>({
         { -1.5f, +1.9f, 0.0f },
         { -1.5f, -2.6f, 0.0f },
         { +1.5f, +1.9f, 0.0f },
         { +1.5f, -2.6f, 0.0f },
         { -1.5f,  0.0f, 0.0f },
         { +1.5f,  0.0f, 0.0f },
+    });
+
+    static constexpr auto SPECIAL_COP_PLACEMENTS = std::to_array<CVector>({
         {  0.0f, +3.2f, 0.0f },
         { +1.5f, -1.8f, 0.0f },
         {  0.0f, +3.2f, 0.0f },
         { -1.5f, -1.8f, 0.0f },
         { -1.5f,  0.0f, 0.0f },
-        { +1.5f,  0.0f, 0.0f }
-    };
+        { +1.5f,  0.0f, 0.0f },
+    });
+
+    const auto placementIdx = 2 * pedsPositionsType;
+    const auto radiusRatio  = vehicle->GetColModel()->GetBoundingSphere().m_fRadius
+        / CModelInfo::GetModelInfo(CStreaming::GetDefaultCopCarModel(false))->GetColModel()->GetBoundingSphere().m_fRadius;
 
     for (auto i = 0u; i < vehicle->m_nNumPedsForRoadBlock; i++) {
-        // ...
+        const auto point = (isSpecialCop ? SPECIAL_COP_PLACEMENTS : PLACEMENTS)[placementIdx + i] * radiusRatio;
+        auto pointWithCar = vehicle->GetMatrix().TransformPoint(point);
+
+        auto* ped = [&]() -> CPed* {
+            if (type != PED_TYPE_COP) {
+                // 0x461560
+                return new CCivilianPed(type, pedModel);
+            } else {
+                auto* p = new CCopPed(CStreaming::IsModelLoaded(pedModel) ? copType : COP_TYPE_CITYCOP);
+                if (copType == COP_TYPE_CITYCOP) {
+                    p->SetCurrentWeapon(WEAPON_PISTOL);
+                }
+                return p;
+            }
+        }();
+
+        pointWithCar = CPedPlacement::FindZCoorForPed(pointWithCar).first;
+        ped->SetPosn(pointWithCar);
+        ped->GetMatrix().SetRotateKeepPos({ 0.0f, 0.0f, -HALF_PI });
+
+        if (type == PED_TYPE_COP) {
+            auto* t = new CTaskComplexWanderCop(PEDMOVE_STILL, CGeneral::GetRandomNumberInRange(8u));
+            t->m_nSubTaskCreatedTimer = {};
+            t->m_nScanForStuffTimer   = {};
+            ped->GetTaskManager().SetTask(t, TASK_PRIMARY_PRIMARY);
+        }
+        ped->GetTaskManager().SetTask(new CTaskSimpleStandStill(0, true), TASK_PRIMARY_DEFAULT);
+
+        ped->bStayInSamePlace         = true;
+        ped->bNotAllowedToDuck        = true;
+        ped->m_nTimeTillWeNeedThisPed = CTimer::GetTimeInMS() + 10'000;
+        ped->bCrouchWhenShooting      = !isSpecialCop || pedsPositionsType != 2;
+        ped->bCullExtraFarAway        = true;
+        CEntity::RegisterReference(ped->m_pVehicle = vehicle);
+        CVisibilityPlugins::SetClumpAlpha(ped->m_pRwClump, 0);
+
+        if (type != PED_TYPE_COP) {
+            // CGangInfo::GetRandomWeapon() wouldn't work here because it filters
+            // free slots. We choose a random every slot regardless if it's empty or not.
+            const auto weapon = CGeneral::RandomChoice(CGangs::Gang[type - PED_TYPE_GANG1].m_nGangWeapons);
+            if (weapon != WEAPON_UNARMED) {
+                ped->GiveDelayedWeapon(weapon, 25'001);
+                ped->SetCurrentWeapon(weapon);
+            }
+        }
+        CWorld::Add(ped);
+        ped->GetEventGroup().Add<CEventScriptCommand>({ TASK_PRIMARY_PRIMARY, new CTaskComplexKillPedOnFoot(FindPlayerPed()) });
     }
 }
 
