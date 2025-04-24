50454fbfcdd80e3b60d46152c2c19958f1e6092c
boludoz <francomaro@gmail.com>
2025-04-24 13:16:51 -0300

    GTATranslateShiftKey

--- source/game_sa/ControllerConfigManager.cpp
+++ source/game_sa/ControllerConfigManager.cpp
@@ -1,6 +1,10 @@
 #include "StdInc.h"
 #include "WinInput.h"
 
+#ifndef NOTSA_USE_SDL3
+#include "WndProc.h"
+#endif
+
 #include "ControllerConfigManager.h"
 
 CControllerConfigManager& ControlsManager = *(CControllerConfigManager *) 0xB70198;
@@ -1152,10 +1156,10 @@ void CControllerConfigManager::MakeControllerActionsBlank() {
 
 // 0x531140
 void CControllerConfigManager::AffectPadFromKeyBoard() {
-// #ifndef NOTSA_USE_SDL3
-//     RsKeyCodes keyCode;
-//     GTATranslateShiftKey(&keyCode); // TODO: It bugs the build with DINPUT and i am too lazy to fix it 
-// #endif
+#ifndef NOTSA_USE_SDL3
+    RsKeyCodes keyCode;
+    GTATranslateShiftKey(&keyCode);
+#endif
 
     const auto inMenu = !CPad::padNumber && !FrontEndMenuManager.m_bMenuActive;
     for (auto& action : m_Actions) {
