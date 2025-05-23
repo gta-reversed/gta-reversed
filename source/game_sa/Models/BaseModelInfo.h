ecaa499110aa40f92a129740f7d82c0a450a0bef
Vladik01-11 <denjidananaa@bk.ru>
2025-05-23 21:04:11 +0300

    fix build

--- source/game_sa/Models/BaseModelInfo.h
+++ source/game_sa/Models/BaseModelInfo.h
@@ -153,8 +153,6 @@ public:
     CPedModelInfo*     AsPedModelInfoPtr()     { return reinterpret_cast<CPedModelInfo*>(this); }
     CWeaponModelInfo*  AsWeaponModelInfoPtr()  { return reinterpret_cast<CWeaponModelInfo*>(this); }
 
-    // 0x4082F0 & 0x424200
-    static CColModel* GetColModel(int32 modelIndex) { return CModelInfo::GetModelInfo(modelIndex)->m_pColModel; }
     [[nodiscard]] CColModel* GetColModel() const { return m_pColModel; }
 
     [[nodiscard]] bool GetIsDrawLast() const { return bDrawLast; }
