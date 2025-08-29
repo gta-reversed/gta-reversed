afbea1e326e776219996b34d517061c495a3c482
Vladik01-11 <denjidananaa@bk.ru>
2025-08-30 00:00:15 +0300

    apply notes

--- source/game_sa/Door.cpp
+++ source/game_sa/Door.cpp
@@ -32,44 +32,44 @@ bool CDoor::ProcessImpact(CVehicle* vehicle, CVector& oldMoveSpeed, CVector& old
 
 // 0x6F4790
 void CDoor::Open(float angRatio) {
-    m_fPrevAngle = m_fAngle;
+    m_prevAngle = m_angle;
     if (angRatio >= 1.0f) {
-        m_nDirn  = m_nDirn;
-        m_fAngle = m_fOpenAngle;
-        if (m_nDirn >= 0) {
-            m_nDoorState = DOOR_HIT_MAX_END;
+        m_dirn  = m_dirn;
+        m_angle = m_openAngle;
+        if (m_dirn >= 0) {
+            m_doorState = DOOR_HIT_MAX_END;
         }
     } else {
-        m_fAngle = angRatio * m_fOpenAngle;
-        if (m_fAngle == 0.0f) {
-            m_fAngVel = 0.0f;
+        m_angle = angRatio * m_openAngle;
+        if (m_angle == 0.0f) {
+            m_angVel = 0.0f;
         }
     }
 }
 
 // 0x6F47E0
 float CDoor::GetAngleOpenRatio() const {
-    if (m_fOpenAngle == 0.0f) {
+    if (m_openAngle == 0.0f) {
         return 0.0f;
     } else {
-        return m_fAngle / m_fOpenAngle;
+        return m_angle / m_openAngle;
     }
 }
 
 // 0x6F4800
 bool CDoor::IsClosed() const {
-    return m_fClosedAngle == m_fAngle;
+    return m_closedAngle == m_angle;
 }
 
 // 0x6F4820
 bool CDoor::IsFullyOpen() const {
-    return fabs(m_fOpenAngle) - 0.5f <= fabs(m_fAngle);
+    return fabs(m_openAngle) - 0.5f <= fabs(m_angle);
 }
 
 // NOTSA
 CVector CDoor::GetRotation() const {
     CVector rotation{ 0.f, 0.f, 0.f };
-    rotation[m_nAxis] = m_fAngle;
+    rotation[m_axis] = m_angle;
     return rotation;
 }
 
--- source/game_sa/Door.h
+++ source/game_sa/Door.h
@@ -20,16 +20,16 @@ enum eDoorState : uint8 {
 
 class CDoor {
 public:
-    float      m_fOpenAngle{};
-    float      m_fClosedAngle{};
+    float      m_openAngle{};
+    float      m_closedAngle{};
 
-    uint16     m_nDirn{}; // Def a bitfield. 2 x 4 bits + some more
-    uint8      m_nAxis{}; // X, Y, Z - Right/left, front/back, up/down
-    eDoorState m_nDoorState{};
+    uint16     m_dirn{}; // Def a bitfield. 2 x 4 bits + some more
+    uint8      m_axis{}; // X, Y, Z - Right/left, front/back, up/down
+    eDoorState m_doorState{};
 
-    float      m_fAngle{};
-    float      m_fPrevAngle{};
-    float      m_fAngVel{};    // Angular velocity
+    float      m_angle{};
+    float      m_prevAngle{};
+    float      m_angVel{};    // Angular velocity
 
     static constexpr float DOOR_SPEED_MAX_CAPPED = 0.5f; // 0x8D3950
 
@@ -38,20 +38,20 @@ public:
     bool ProcessImpact(CVehicle* vehicle, CVector& vecOldMoveSpeed, CVector& vecOldTurnSpeed, Const CVector& vecOffset);
     void Open(float angRatio);
 
-    float RetDoorAngVel() const { return m_fAngVel; }
-    float RetDoorAngle() const { return m_fAngle; }
-    float RetAngleWhenClosed() const { return m_fClosedAngle; }
-    float RetAngleWhenOpen() const { return m_fOpenAngle; }
+    float RetDoorAngVel() const { return m_angVel; }
+    float RetDoorAngle() const { return m_angle; }
+    float RetAngleWhenClosed() const { return m_closedAngle; }
+    float RetAngleWhenOpen() const { return m_openAngle; }
     float GetAngleOpenRatio() const;
     bool  IsClosed() const;
     bool  IsFullyOpen() const;
 
-    uint8 RetAxes() const { return m_nAxis; }
-    uint8 RetDoorState() const { return m_nDoorState; }
+    uint8 RetAxes() const { return m_axis; }
+    uint8 RetDoorState() const { return m_doorState; }
 
     // NOTSA
-    void    OpenFully()  { m_fAngle = m_fOpenAngle; }
-    void    CloseFully() { m_fAngle = m_fClosedAngle; }
+    void    OpenFully()  { m_angle = m_openAngle; }
+    void    CloseFully() { m_angle = m_closedAngle; }
     CVector GetRotation() const;
     void    UpdateFrameMatrix(CMatrix& mat);
     static void InjectHooks();
