ef83bf482efb38170e81d2b1936ab55fd6217814
Vladik01-11 <denjidananaa@bk.ru>
2025-08-29 23:42:40 +0300

    commit

--- source/game_sa/Door.cpp
+++ source/game_sa/Door.cpp
@@ -8,44 +8,72 @@
 
 #include "Door.h"
 
-float& CDoor::DOOR_SPEED_MAX_CAPPED = *(float*)0x8D3950;
+void CDoor::InjectHooks() {
+    RH_ScopedClass(CDoor);
+    RH_ScopedCategoryGlobal();
+
+    RH_ScopedGlobalInstall(Process, 0x6F4040, { .reversed = false });
+    RH_ScopedGlobalInstall(ProcessImpact, 0x6F4540, { .reversed = false });
+    RH_ScopedGlobalInstall(Open, 0x6F4790);
+    RH_ScopedGlobalInstall(GetAngleOpenRatio, 0x6F47E0);
+    RH_ScopedGlobalInstall(IsClosed, 0x6F4800);
+    RH_ScopedGlobalInstall(IsFullyOpen, 0x6F4820);
+}
 
 // 0x6F4040
-bool CDoor::Process(CVehicle* vehicle, CVector& arg1, CVector& arg2, Const CVector& thisDoorPos) {
-    return plugin::CallMethodAndReturn<bool, 0x6F4040, CDoor*, CVehicle*, CVector&, CVector&, CVector&>(this, vehicle, arg1, arg2, const_cast<CVector&>(thisDoorPos));
+bool CDoor::Process(CVehicle* vehicle, CVector& oldMoveSpeed, CVector& oldTurnSpeed, Const CVector& offset) {
+    return plugin::CallMethodAndReturn<bool, 0x6F4040, CDoor*, CVehicle*, CVector&, CVector&, CVector&>(this, vehicle, oldMoveSpeed, oldTurnSpeed, const_cast<CVector&>(offset));
 }
 
 // 0x6F4540
-bool CDoor::ProcessImpact(CVehicle* vehicle, CVector& arg1, CVector& arg2, Const CVector& thisDoorPos) {
-    return plugin::CallMethodAndReturn<bool, 0x6F4540, CDoor*, CVehicle*, CVector&, CVector&, CVector&>(this, vehicle, arg1, arg2, const_cast<CVector&>(thisDoorPos));
+bool CDoor::ProcessImpact(CVehicle* vehicle, CVector& oldMoveSpeed, CVector& oldTurnSpeed, Const CVector& offset) {
+    return plugin::CallMethodAndReturn<bool, 0x6F4540, CDoor*, CVehicle*, CVector&, CVector&, CVector&>(this, vehicle, oldMoveSpeed, oldTurnSpeed, const_cast<CVector&>(offset));
 }
 
 // 0x6F4790
-void CDoor::Open(float openRatio) {
-    plugin::CallMethod<0x6F4790, CDoor*, float>(this, openRatio);
+void CDoor::Open(float angRatio) {
+    m_fPrevAngle = m_fAngle;
+    if (angRatio >= 1.0f) {
+        m_nDirn  = m_nDirn;
+        m_fAngle = m_fOpenAngle;
+        if (m_nDirn >= 0) {
+            m_nDoorState = DOOR_HIT_MAX_END;
+        }
+    } else {
+        m_fAngle = angRatio * m_fOpenAngle;
+        if (m_fAngle == 0.0f) {
+            m_fAngVel = 0.0f;
+        }
+    }
 }
 
 // 0x6F47E0
-float CDoor::GetAngleOpenRatio() {
-    return plugin::CallMethodAndReturn<float, 0x6F47E0, CDoor*>(this);
+float CDoor::GetAngleOpenRatio() const {
+    if (m_fOpenAngle == 0.0f) {
+        return 0.0f;
+    } else {
+        return m_fAngle / m_fOpenAngle;
+    }
 }
 
 // 0x6F4800
-bool CDoor::IsClosed() {
-    return plugin::CallMethodAndReturn<bool, 0x6F4800, CDoor*>(this);
+bool CDoor::IsClosed() const {
+    return m_fClosedAngle == m_fAngle;
 }
 
 // 0x6F4820
-bool CDoor::IsFullyOpen() {
-    return plugin::CallMethodAndReturn<bool, 0x6F4820, CDoor*>(this);
+bool CDoor::IsFullyOpen() const {
+    return fabs(m_fOpenAngle) - 0.5f <= fabs(m_fAngle);
 }
 
+// NOTSA
 CVector CDoor::GetRotation() const {
     CVector rotation{ 0.f, 0.f, 0.f };
     rotation[m_nAxis] = m_fAngle;
     return rotation;
 }
 
+// NOTSA
 void CDoor::UpdateFrameMatrix(CMatrix& mat) {
     mat.SetRotateKeepPos(GetRotation());
     mat.UpdateRW();
--- source/game_sa/Door.h
+++ source/game_sa/Door.h
@@ -22,28 +22,39 @@ class CDoor {
 public:
     float      m_fOpenAngle;
     float      m_fClosedAngle;
-    int16      m_nDirn; // Def a bitfield. 2 x 4 bits + some more
+
+    uint16     m_nDirn; // Def a bitfield. 2 x 4 bits + some more
     uint8      m_nAxis; // X, Y, Z - Right/left, front/back, up/down
     eDoorState m_nDoorState;
+
     float      m_fAngle;
     float      m_fPrevAngle;
     float      m_fAngVel;    // Angular velocity
 
-    static float& DOOR_SPEED_MAX_CAPPED; // 0.5f
+    static constexpr float DOOR_SPEED_MAX_CAPPED = 0.5f; // 0x8D3950
 
 public:
-    bool  Process(CVehicle* vehicle, CVector& arg1, CVector& arg2, Const CVector& arg3);
-    bool  ProcessImpact(CVehicle* vehicle, CVector& arg1, CVector& arg2, Const CVector& arg3);
-    void  Open(float openRatio);
-    float GetAngleOpenRatio();
-    bool  IsClosed();
-    bool  IsFullyOpen();
+    bool Process(CVehicle* vehicle, CVector& vecOldMoveSpeed, CVector& vecOldTurnSpeed, Const CVector& vecOffset);
+    bool ProcessImpact(CVehicle* vehicle, CVector& vecOldMoveSpeed, CVector& vecOldTurnSpeed, Const CVector& vecOffset);
+    void Open(float angRatio);
+
+    float RetDoorAngVel() const { return m_fAngVel; }
+    float RetDoorAngle() const { return m_fAngle; }
+    float RetAngleWhenClosed() const { return m_fClosedAngle; }
+    float RetAngleWhenOpen() const { return m_fOpenAngle; }
+    float GetAngleOpenRatio() const;
+    bool  IsClosed() const;
+    bool  IsFullyOpen() const;
+
+    uint8 RetAxes() const { return m_nAxis; }
+    uint8 RetDoorState() const { return m_nDoorState; }
 
     // NOTSA
     void    OpenFully()  { m_fAngle = m_fOpenAngle; }
     void    CloseFully() { m_fAngle = m_fClosedAngle; }
     CVector GetRotation() const;
     void    UpdateFrameMatrix(CMatrix& mat);
+    static void InjectHooks();
 };
 
 VALIDATE_SIZE(CDoor, 0x18);
