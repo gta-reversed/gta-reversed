0ea3c20b3b252a76d9d1f6df2b809a9a1dccbc2a
MrX <mrxreverse@gmail.com>
2026-01-18 23:40:18 -0300

    Implement `GetHeadPtr` in CEntryInfoList
    Add the GetHeadPtr method to the CEntryInfoList list; it is originally inlined.

--- source/game_sa/Core/EntryInfoList.h
+++ source/game_sa/Core/EntryInfoList.h
@@ -12,6 +12,9 @@ public:
     void Flush();                          // 0x536E10
     void DeleteNode(CEntryInfoNode* node); // Most likely inlined in final exe
     bool IsEmpty() const noexcept { return !m_node; }
+
+    // inlined
+    CEntryInfoNode* GetHeadPtr() { return m_node; }
 };
 
 VALIDATE_SIZE(CEntryInfoList, 0x4);
