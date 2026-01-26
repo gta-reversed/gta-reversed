ecc770f4d9d98ee9cc6361c7af85ed397c77cb3c
MrX <mrxreverse@gmail.com>
2026-01-26 00:25:03 -0300

    Update Vehicle.cpp

--- source/game_sa/Entity/Vehicle/Vehicle.cpp
+++ source/game_sa/Entity/Vehicle/Vehicle.cpp
@@ -1210,9 +1210,9 @@ void CVehicle::CalculateLightingFromCollision() {
 void CVehicle::ResetAfterRender() {
     RwRenderStateSet(RwRenderState::rwRENDERSTATECULLMODE, RWRSTATE(rwCULLMODECULLBACK));
     CVehicleModelInfo::ResetEditableMaterials((RpClump*)GetRwObject());
+             
     if (IsAutomobile()) {
         auto* const automobile = static_cast<CAutomobile*>(this);
-
         auto* const mi = automobile->GetModelInfo()->AsVehicleModelInfoPtr();
         assert(mi != nullptr);
         automobile->CustomCarPlate_AfterRenderingStop(mi);
