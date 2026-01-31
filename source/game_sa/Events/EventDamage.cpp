523762843c9f31b888d8269cacd9d258ea5f7d42
Dryxio <momemtumcorpfzco@gmail.com>
2026-01-31 02:44:51 +0100

    Fix punching ped make them fall

--- source/game_sa/Events/EventDamage.cpp
+++ source/game_sa/Events/EventDamage.cpp
@@ -46,15 +46,17 @@ CEventDamage::CEventDamage(CEntity* source, uint32 startTime, eWeaponType weapon
     m_weaponType            = weaponType;
     m_pedPieceType          = pieceHit;
     m_ucDirection           = direction;
-    m_bWitnessedInVehicle   = bPedInVehicle;
     m_bJumpedOutOfMovingCar = bJumpedOutOfMovingCar;
+    m_bFallDown             = false;
+    m_bAnimAdded            = false;
+    m_bWitnessedInVehicle   = bPedInVehicle;
+    m_bStealthMode          = false;
     m_nAnimGroup            = ANIM_GROUP_DEFAULT;
     m_nAnimID               = ANIM_ID_NO_ANIMATION_SET;
     m_fAnimBlend            = 8.0f;
     m_fAnimSpeed            = 1.0f;
 
     CEntity::SafeRegisterRef(m_pSourceEntity);
-    m_bWitnessedInVehicle = true; // if we're setting this to true, then why do we have bPedInVehicle parameter in this constructor? bug?
 }
 
 CEventDamage::~CEventDamage() {
@@ -633,6 +635,7 @@ void CEventDamage::ComputeDeathAnim(CPed* ped, bool bMakeActiveTaskAbortable) {
 
 // 0x4B3FC0
 void CEventDamage::ComputeDamageAnim(CPed* ped, bool bMakeActiveTaskAbortable) {
+    m_bFallDown = false; // Vanilla clears the fall-down flag during anim selection
     if (ped->bInVehicle && ped->m_pVehicle) {
         CWeaponInfo* pWeaponInfo = CWeaponInfo::GetWeaponInfo((eWeaponType)this->m_weaponType, eWeaponSkill::STD);
         if (!pWeaponInfo->m_nWeaponFire
@@ -908,7 +911,7 @@ void CEventDamage::ComputeAnim(CPed* ped, bool bMakeActiveTaskAbortable) {
     if (ped->m_fHealth <= 0.f) {
         ComputeDeathAnim(ped, bMakeActiveTaskAbortable);
     } else {
-        ComputeDeathAnim(ped, bMakeActiveTaskAbortable);
+        ComputeDamageAnim(ped, bMakeActiveTaskAbortable);
     }
 }
 
