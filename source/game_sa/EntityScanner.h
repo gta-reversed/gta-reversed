adb18e41d9a510e4229348e0b33a17c91d23ea6e
Vladik01-11 <denjidananaa@bk.ru>
2026-02-16 13:55:30 +0300

    Reversed `CEntityScanner` and `CTickCounter`

--- source/game_sa/EntityScanner.h
+++ source/game_sa/EntityScanner.h
@@ -6,30 +6,56 @@
 */
 #pragma once
 
-#include <Base.h>
 #include "RepeatSector.h"
+#include "TickCounter.h"
 #include <extensions/utility.hpp>
 
 class CEntity;
 class CPed;
+
+static constexpr size_t MAX_NUM_ENTITIES = 16; // Mobile
 
 // Just a theory..
 // Since this is originally a virtual class..
 // Maybe it was templated? Like: `template<class Entity_t, size_t MaxCount>` ?
 
 class CEntityScanner {
-public:
-    int32    field_4;
-    uint32   m_nCount;
-    std::array<CEntity*, 16> m_apEntities; /// SEEMINGLY: The array might have "holes" in it, also it's sorted by distance (closer to further)
+protected:
+    CTickCounter m_timer;
+    std::array<CEntity*, MAX_NUM_ENTITIES> m_apEntities; /// SEEMINGLY: The array might have "holes" in it, also it's sorted by distance (closer to further)
     CEntity* m_pClosestEntityInRange;
 
 public:
-    static void InjectHooks();
+    static const int32 ms_iScanPeriod = 10; // Mobile, unused
 
+public:
     CEntityScanner();
     ~CEntityScanner();
 
+    CEntity** GetEntitiesInRange() { return m_apEntities.data(); }
+
+    CTickCounter* GetTimer() { return &m_timer; } // unused
+
+protected:
+    virtual void ScanForEntitiesInRange(const eRepeatSectorList sectorList, const CPed& ped);
+    CEntity* GetClosestEntity() const { return m_pClosestEntityInRange; }
+    void Clear();
+
+private: // NOTSA:
+    friend void InjectHooksMain();
+    static void InjectHooks();
+
+    CEntityScanner* Constructor() {
+        this->CEntityScanner::CEntityScanner();
+        return this;
+    }
+
+    CEntityScanner* Destructor() {
+        this->CEntityScanner::~CEntityScanner();
+        return this;
+    }
+
+public:
     /// View of all non-null entities as a view of `T&`
     template<typename T = CEntity>
     auto GetEntities() const {
@@ -40,9 +66,6 @@ public:
              | filter([](auto&& e) { return e != nullptr; })  // Filter all null
              | transform([](CEntity* e) -> T& { return static_cast<T&>(*e); }); // Cast to required type and dereference
     }
-
-    void Clear();
-    virtual void ScanForEntitiesInRange(eRepeatSectorList sectorList, const CPed& ped);
 };
 
 VALIDATE_SIZE(CEntityScanner, 0x50);
