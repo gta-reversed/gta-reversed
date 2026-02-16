4adcb0c6e234c9351cd988b79ff44c18a07d5039
MrX <mrxreverse@gmail.com>
2026-02-16 20:28:56 -0300

    Fix static_cast for automobile in Vehicle.cpp

--- source/game_sa/Entity/Vehicle/Vehicle.cpp
+++ source/game_sa/Entity/Vehicle/Vehicle.cpp
@@ -1212,7 +1212,7 @@ void CVehicle::ResetAfterRender() {
     CVehicleModelInfo::ResetEditableMaterials((RpClump*)GetRwObject());
 
     if (IsAutomobile()) {
-        auto* const automobile = (CAutomobile*)(this);
+        auto* const automobile = static_cast<CAutomobile*>(this);
         auto* const mi = GetVehicleModelInfo();
         assert(mi != nullptr);
         automobile->CustomCarPlate_AfterRenderingStop(mi);
