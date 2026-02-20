0f734b6e41b79f67b4ede1cdb1d98085b6474135
Pirulax <patrikjankovics7@gmail.com>
2026-02-20 03:49:47 +0100

    `COMMAND_SET_CAN_BURST_CAR_TYRES`

--- source/game_sa/Scripts/Commands/VehicleCommands.cpp
+++ source/game_sa/Scripts/Commands/VehicleCommands.cpp
@@ -1230,9 +1230,9 @@ bool HasCarBeenDamagedByCar(CVehicle* self, CVehicle* other) {
 * @param self CVehicle&
 * @param state bool
 */
-// void SetCanBurstCarTyres(CVehicle& self, bool state) {
-//     NOTSA_UNREACHABLE("Not implemented");
-// }
+void SetCanBurstCarTyres(CVehicle& self, bool state) {
+    self.vehicleFlags.bTyresDontBurst = state;
+}
 
 /*
 * @opcode 054F
@@ -2580,7 +2580,7 @@ void notsa::script::commands::vehicle::RegisterHandlers() {
     REGISTER_COMMAND_HANDLER(COMMAND_FREEZE_CAR_POSITION, FreezeCarPosition);
     REGISTER_COMMAND_HANDLER(COMMAND_HAS_CAR_BEEN_DAMAGED_BY_CHAR, HasCarBeenDamagedByChar);
     REGISTER_COMMAND_HANDLER(COMMAND_HAS_CAR_BEEN_DAMAGED_BY_CAR, HasCarBeenDamagedByCar);
-    //REGISTER_COMMAND_HANDLER(COMMAND_SET_CAN_BURST_CAR_TYRES, SetCanBurstCarTyres);
+    REGISTER_COMMAND_HANDLER(COMMAND_SET_CAN_BURST_CAR_TYRES, SetCanBurstCarTyres);
     //REGISTER_COMMAND_HANDLER(COMMAND_CLEAR_CAR_LAST_DAMAGE_ENTITY, ClearCarLastDamageEntity);
     //REGISTER_COMMAND_HANDLER(COMMAND_FREEZE_CAR_POSITION_AND_DONT_LOAD_COLLISION, FreezeCarPositionAndDontLoadCollision);
     //REGISTER_COMMAND_HANDLER(COMMAND_SET_LOAD_COLLISION_FOR_CAR_FLAG, SetLoadCollisionForCarFlag);
