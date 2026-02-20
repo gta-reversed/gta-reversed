89d3d490318706ed45b8588dd7922f0b89908154
Pirulax <patrikjankovics7@gmail.com>
2026-02-20 19:06:00 +0100

    `COMMAND_GET_CAR_ROLL`

--- source/game_sa/Scripts/Commands/VehicleCommands.cpp
+++ source/game_sa/Scripts/Commands/VehicleCommands.cpp
@@ -1696,9 +1696,9 @@ float GetCarMass(CVehicle& self) {
 * 
 * @param self CVehicle&
 */
-// float GetCarRoll(CVehicle& self) {
-//     NOTSA_UNREACHABLE("Not implemented");
-// }
+float GetCarRoll(CVehicle& self) {
+    return self.GetRoll();
+}
 
 /*
 * @opcode 06C5
@@ -2657,7 +2657,7 @@ void notsa::script::commands::vehicle::RegisterHandlers() {
     REGISTER_COMMAND_HANDLER(COMMAND_FIX_CAR_TYRE, FixCarTyre);
     REGISTER_COMMAND_HANDLER(COMMAND_GET_CAR_SPEED_VECTOR, GetCarSpeedVector);
     REGISTER_COMMAND_HANDLER(COMMAND_GET_CAR_MASS, GetCarMass);
-    //REGISTER_COMMAND_HANDLER(COMMAND_GET_CAR_ROLL, GetCarRoll);
+    REGISTER_COMMAND_HANDLER(COMMAND_GET_CAR_ROLL, GetCarRoll);
     //REGISTER_COMMAND_HANDLER(COMMAND_SKIP_TO_END_AND_STOP_PLAYBACK_RECORDED_CAR, SkipToEndAndStopPlaybackRecordedCar);
     //REGISTER_COMMAND_HANDLER(COMMAND_DOES_CAR_HAVE_STUCK_CAR_CHECK, DoesCarHaveStuckCarCheck);
     //REGISTER_COMMAND_HANDLER(COMMAND_CAR_GOTO_COORDINATES_RACING, CarGotoCoordinatesRacing);
