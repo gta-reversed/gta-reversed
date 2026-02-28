93faef4b82e107576ec0dea8d25a0462927dd2f9
Pirulax <patrikjankovics7@gmail.com>
2026-02-28 03:07:28 +0100

    Move `Model` and `Hash` into a separate `Typeshed.hpp` file from `ReadArg.hpp`

--- source/game_sa/Scripts/CommandParser/ReadArg.hpp
+++ source/game_sa/Scripts/CommandParser/ReadArg.hpp
@@ -11,28 +11,10 @@
 #include "Rect.h"
 #include "Scripted2dEffects.h"
 
+#include "./Typeshed.hpp"
+
 namespace notsa {
 namespace script {
-namespace detail {
-
-template<typename T, size_t UniqueTag = 0>
-struct StrongAlias {
-    T value;
-
-    auto& operator=(const T& o) {
-        value = o;
-        return *this;
-    }
-
-    operator T() const { return value; }
-};
-};
-
-// eModelID wrapper that is read either from a static int32 value or UsedObjectArray.
-using Model = detail::StrongAlias<eModelID>;
-// uint32 wrapper for reading an unsigned 32-bit integer that can be out of range for int32
-using Hash = detail::StrongAlias<uint32>;
-
 namespace detail {
 
 //! Script thing bullshittery
