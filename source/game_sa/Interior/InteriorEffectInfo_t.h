49bb57b873300bd1d52b6aec92c57ee5925bdf67
Vladik01-11 <denjidananaa@bk.ru>
2026-03-04 23:18:01 +0300

    InteriorEffectInfo_t

--- source/game_sa/Interior/InteriorEffectInfo_t.h
+++ source/game_sa/Interior/InteriorEffectInfo_t.h
@@ -5,14 +5,17 @@
 #include "Vector.h"
 
 struct C2dEffectInterior;
-class  CEntity;
+class CEntity;
 
 struct InteriorEffectInfo_t {
-    CEntity*                          Entity;
+    CEntity* Entity;
+
     size_t                            NumFx;
     std::array<C2dEffectInterior*, 8> Effects;
     std::array<uint32, 8>             FxIds;
-    float                             DistSq; ///< Min dist. sq. to camera from any of the effects (in the `Effects` array)
-    bool                              IsCulled;
+
+    float DistSq; ///< Min dist. sq. to camera from any of the effects (in the `Effects` array)
+    bool  IsCulled;
 };
+
 VALIDATE_SIZE(InteriorEffectInfo_t, 0x50);
