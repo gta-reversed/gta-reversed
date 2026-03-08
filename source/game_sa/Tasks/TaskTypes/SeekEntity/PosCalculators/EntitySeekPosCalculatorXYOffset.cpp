e0316daf1b4c124b031324db90e578d8fa7b2c39
weikton <weikton@vk.com>
2026-03-08 15:56:37 +0200

    Reverse `CEntitySeekPosCalculatorXYOffset::ComputeEntitySeekPos`

--- source/game_sa/Tasks/TaskTypes/SeekEntity/PosCalculators/EntitySeekPosCalculatorXYOffset.cpp
+++ source/game_sa/Tasks/TaskTypes/SeekEntity/PosCalculators/EntitySeekPosCalculatorXYOffset.cpp
@@ -6,9 +6,9 @@ void CEntitySeekPosCalculatorXYOffset::InjectHooks() {
     RH_ScopedVirtualClass(CEntitySeekPosCalculatorXYOffset, 0x86F8F8, 1);
     RH_ScopedCategory("Tasks/TaskTypes/SeekPosCalculators");
 
-    RH_ScopedVMTInstall(ComputeEntitySeekPos, 0x6946a0, { .reversed = false });
+    RH_ScopedVMTInstall(ComputeEntitySeekPos, 0x6946a0);
 }
 
-void CEntitySeekPosCalculatorXYOffset::ComputeEntitySeekPos(const CPed& seeker, const CEntity& target, CVector& outPos) {
-    return plugin::CallMethod<0x6946a0, CEntitySeekPosCalculatorXYOffset*, const CPed&, const CEntity&, CVector&>(this, seeker, target, outPos);
+void CEntitySeekPosCalculatorXYOffset::ComputeEntitySeekPos(const CPed&, const CEntity& target, CVector& outPos) {
+    outPos = target.GetMatrix().TransformPoint(m_offsetXY);
 }
