f4ad1269892e72ee4a6e1b18d36583d781f27afd
Vladik01-11 <70571714+Vladik01-11@users.noreply.github.com>
2026-03-21 00:48:47 +0300

    `CDoor`, fix `CVehicle::ProcessOpenDoor`, `FinishAnimCarOpenDoorFromOutsideCB` (#1240)
    Co-authored-by: yukani <kurbagatengu@gmail.com>

--- source/game_sa/Entity/Vehicle/Boat.cpp
+++ source/game_sa/Entity/Vehicle/Boat.cpp
@@ -116,15 +116,10 @@ CBoat::CBoat(int32 modelIndex, eVehicleCreatedBy createdBy) : CVehicle(createdBy
     rng::fill(m_WakePtCounters, 0.0f);
 
     m_nAmmoInClip = 20;
-    m_BoatDoor.m_axis = AXIS_Y;
     if (m_nModelIndex == MODEL_MARQUIS) {
-        m_BoatDoor.m_openAngle = PI / 10.0F;
-        m_BoatDoor.m_closedAngle = -PI / 10.0F;
-        m_BoatDoor.m_dirn = 4;
+        m_BoatDoor.Init(PI / 10.0F, -PI / 10.0F, DOOR_AXIS_NEG_Y, DOOR_AXIS_Y, DOOR_EXTRA_NONE);
     } else {
-        m_BoatDoor.m_openAngle = TWO_PI / 10.0F;
-        m_BoatDoor.m_closedAngle = -TWO_PI / 10.0F;
-        m_BoatDoor.m_dirn = 3;
+        m_BoatDoor.Init(TWO_PI / 10.0F, -TWO_PI / 10.0F, DOOR_AXIS_NEG_X, DOOR_AXIS_Y, DOOR_EXTRA_NONE);
     }
 
     m_vehicleAudio.Initialise(this);
@@ -745,7 +740,7 @@ void CBoat::PreRender() {
 
             m_BoatDoor.Process(this, m_OldMoveSpeed, m_OldTurnSpeed, vecTransformed);
             CVector vecAxis;
-            vecAxis[m_BoatDoor.m_axis] = m_BoatDoor.m_angle;
+            vecAxis[m_BoatDoor.GetAxes()] = m_BoatDoor.m_angle;
 
             tempMat.SetRotate(vecAxis.x, vecAxis.y, vecAxis.z);
             tempMat.GetPosition() += posCopy;
