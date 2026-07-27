21c837c81efa8ed0381e9d52fac805ff9ae519cb
Pirulax <patrikjankovics7@gmail.com>
2026-07-28 00:54:27 +0200

    add warn

--- source/game_sa/TagManager.cpp
+++ source/game_sa/TagManager.cpp
@@ -73,6 +73,7 @@ tTagDesc* CTagManager::FindTagDesc(const CEntity& entity) {
             return &tag;
         }
     }
+    NOTSA_LOG_WARN("CTagManager::FindTagDesc: Couldn't find tag desc for entity %p - This shouldn't happen! Perhaps corrupted savefile?", &entity);
     return nullptr;
 }
 
