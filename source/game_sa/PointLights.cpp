3475c4b1526fe7eeedfd5e83c5cb684217378db3
natsuka-shiine <natsukashiine@proton.me>
2026-09-07 00:23:32 +0530

    re(CPointLights): Reverse AddLight

--- source/game_sa/PointLights.cpp
+++ source/game_sa/PointLights.cpp
@@ -37,7 +37,40 @@ bool CPointLights::ProcessVerticalLineUsingCache(CVector point, float* outZ) {
 
 // 0x7000E0
 void CPointLights::AddLight(uint8 lightType, CVector point, CVector direction, float radius, float red, float green, float blue, uint8 fogType, bool generateExtraShadows, CEntity* entityAffected) {
-    plugin::Call<0x7000E0, uint8, CVector, CVector, float, float, float, float, uint8, bool, CEntity*>(lightType, point, direction, radius, red, green, blue, fogType, generateExtraShadows, entityAffected);
+    const CVector delta   = point - TheCamera.GetPosition();
+    const float   maxDist = radius + 15.0f;
+    if (delta.x >= maxDist || -maxDist >= delta.x || delta.y >= maxDist || -maxDist >= delta.y) {
+        return;
+    }
+    if (NumLights >= MAX_POINT_LIGHTS) {
+        return;
+    }
+    const float dist = delta.Magnitude();
+    if (dist >= maxDist) {
+        return;
+    }
+
+    auto& light      = aLights[NumLights++];
+    light.m_nType    = static_cast<ePointLightType>(lightType);
+    light.m_nFogType = fogType;
+
+    light.m_vecPosn      = point;
+    light.m_vecDirection = direction;
+    light.m_fRadius      = radius;
+
+    light.m_bGenerateShadows = generateExtraShadows;
+    light.m_pEntityToLight   = entityAffected;
+
+    // Fade color out starting at 75% of the max distance
+    if (const float fade = 1.0f - (dist / maxDist - 0.75f) * 4.0f; fade < 1.0f) {
+        light.m_fColorRed   = red * fade;
+        light.m_fColorGreen = green * fade;
+        light.m_fColorBlue  = blue * fade;
+    } else {
+        light.m_fColorRed   = red;
+        light.m_fColorGreen = green;
+        light.m_fColorBlue  = blue;
+    }
 }
 
 // 0x7002D0
