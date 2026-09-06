f33bd47b86f561d1ab5f85b1df789fe959643fd3
natsuka-shiine <natsukashiine@proton.me>
2026-09-07 00:25:17 +0530

    re(CPointLights): Reverse GetLightMultiplier

--- source/game_sa/PointLights.cpp
+++ source/game_sa/PointLights.cpp
@@ -14,7 +14,7 @@ void CPointLights::InjectHooks() {
 
     RH_ScopedInstall(Init, 0x6FFB40);
     RH_ScopedInstall(GenerateLightsAffectingObject, 0x6FFBB0, { .reversed = false });
-    RH_ScopedInstall(GetLightMultiplier, 0x6FFE70, { .reversed = false });
+    RH_ScopedInstall(GetLightMultiplier, 0x6FFE70);
     RH_ScopedInstall(RemoveLightsAffectingObject, 0x6FFFE0, { .reversed = false });
     RH_ScopedInstall(ProcessVerticalLineUsingCache, 0x6FFFF0);
     RH_ScopedInstall(AddLight, 0x7000E0);
@@ -35,7 +35,31 @@ float CPointLights::GenerateLightsAffectingObject(const CVector* point, float* t
 
 // 0x6FFE70
 float CPointLights::GetLightMultiplier(const CVector* point) {
-    return plugin::CallAndReturn<float, 0x6FFE70, const CVector*>(point);
+    float antilightMult = 1.0f;
+    float lightSum      = 0.0f;
+    for (const auto& light : GetActiveLights()) {
+        if (light.m_nType == PLTYPE_ONLYFOGEFFECT_ALWAYS || light.m_nType == PLTYPE_ONLYFOGEFFECT) {
+            continue;
+        }
+        const CVector delta = light.m_vecPosn - *point;
+        const float   rad   = light.m_fRadius;
+        if (-rad >= delta.x || delta.x >= rad || -rad >= delta.y || delta.y >= rad || -rad >= delta.z || delta.z >= rad) {
+            continue;
+        }
+        const float dist = delta.Magnitude();
+        if (dist >= rad) {
+            continue;
+        }
+        const float ratio = dist / rad;
+        if (light.m_nType == PLTYPE_ANTILIGHT) {
+            antilightMult *= ratio;
+        } else {
+            lightSum += (1.0f - ratio) * light.m_fColorRed   * (1.0f / 3.0f)
+                      + (1.0f - ratio) * light.m_fColorGreen * (1.0f / 3.0f)
+                      + (1.0f - ratio) * light.m_fColorBlue  * (1.0f / 3.0f);
+        }
+    }
+    return antilightMult + lightSum;
 }
 
 // 0x6FFFE0
