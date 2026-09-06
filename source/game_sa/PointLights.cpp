ddb9d7d124b7592438fc92a712868d2a9fc0a24e
natsuka-shiine <natsukashiine@proton.me>
2026-09-07 00:24:33 +0530

    re(CPointLights): Reverse ProcessVerticalLineUsingCache

--- source/game_sa/PointLights.cpp
+++ source/game_sa/PointLights.cpp
@@ -32,7 +32,25 @@ void CPointLights::RemoveLightsAffectingObject() {
 
 // 0x6FFFF0
 bool CPointLights::ProcessVerticalLineUsingCache(CVector point, float* outZ) {
-    return plugin::CallAndReturn<bool, 0x6FFFF0, CVector, float*>(point, outZ);
+    for (auto&& [i, cached] : rngv::enumerate(aCachedMapReads)) {
+        if (cached.x == point.x && cached.y == point.y && cached.z == point.z) {
+            *outZ = aCachedMapReadResults[i];
+            return true;
+        }
+    }
+
+    CColPoint colPoint;
+    CEntity*  entity;
+    if (!CWorld::ProcessVerticalLine(point, point.z - 5.0f, colPoint, entity, true, false, false, false, true, false, nullptr)) {
+        return false;
+    }
+
+    aCachedMapReadResults[NextCachedValue] = colPoint.m_vecPoint.z;
+    aCachedMapReads[NextCachedValue]       = point;
+    NextCachedValue                        = (NextCachedValue + 1) % MAX_POINT_LIGHTS;
+
+    *outZ = colPoint.m_vecPoint.z;
+    return true;
 }
 
 // 0x7000E0
