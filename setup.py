120fb219a7d12f3f641f19dab7c36e4309a831f9
Pirulax <patrikjankovics7@gmail.com>
2026-09-14 12:16:01 +0200

    fix setup.py as per Yukani's suggestions

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from argparse import ArgumentParser
 import subprocess
 
-ap = ArgumentParser(description="Setup script for the project")
+ap = ArgumentParser(description="Wrapper setup script for the project. All arguments not listed here will be passed to Conan, for more details see Conan's documentation.")
 #ap.add_argument('-o', '--options', dest='options', default=[], nargs='+', help='Options passed to Conan (See `conanfile` for available options)')
 #ap.add_argument('-s', '--settings', dest='settings', default=[], nargs='+', help='Settings passed to Conan (See `conanfile` for available settings)')
-ap.add_argument('-p', '--profile', dest='profile', default='windows-msvc.txt', help='Conan profile to use for the build')
+ap.add_argument('-p', '--profile', dest='profile', default='windows-msvc.txt', help='Conan profile to use for the build - By default it looks for the profile in the `./conan/profiles` directory, but you can also specify a path to a profile file.')
 ap.add_argument('-b', '--build', dest='build', action='store_true', help='Build the project after setup')
 
 def main() -> None:
     args, conan_args = ap.parse_known_args()
 
-    profile = f'./conan/profiles/{args.profile}'
+    profile = args.profile
+    if '/' not in profile:
+        profile = f'./conan/profiles/{profile}'
 
     subprocess.run(["conan", "install", ".", "--build=missing", "--profile", profile, *conan_args], check=True)
     if args.build:
